9390c2718c186f17d5e8fb40a18cb954537bd980
jumpjack <jumpjack@libero.it>
2022-12-21 08:45:20 +0100

    Update painter.js

--- src/painter.js
+++ src/painter.js
@@ -111,10 +111,10 @@ IsoBlock.Painter.prototype = {
 		ctx.fillStyle = color;
 		ctx.fill();
 	},
-	drawText: function(ctx, pos, text, color) {
+	drawText: function(ctx, pos, font, text, color) {
 		var v = this.transform(pos);
-		ctx.lineTo(v.x,v.y);
 		ctx.fillStyle = color;
+		ctx.font = font;
 		ctx.fillText(text, v.x, v.y);		
 	},
 };
